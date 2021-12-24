7cf368dfe4069b00171e8d202d25bed6d1492618
Simon Johnston <johnstonskj@gmail.com>
2021-12-24 10:27:05 -0800

    Added prop tests for predicate, variable, and constant values
    Signed-off-by: Simon Johnston <johnstonskj@gmail.com>

---
+++ tests/prop_tests.rs
@@ -0,0 +1,30 @@
+use asdi::edb::{Constant, Predicate};
+use asdi::idb::Variable;
+use proptest::proptest;
+use std::str::FromStr;
+
+// ------------------------------------------------------------------------------------------------
+// Automated Property Tests
+// ------------------------------------------------------------------------------------------------
+
+proptest! {
+    #[test]
+    fn predicate_from_str(s in r"\p{Ll}[\p{Ll}\p{Lu}\p{Nd}_]*") {
+        println!("predicate_from_str: {:?}", s);
+        assert!(Predicate::is_valid(&s));
+        assert!(Predicate::from_str(&s).is_ok());
+    }
+
+    #[test]
+    fn variable_from_str(s in r"_|\p{Lu}[\p{Ll}\p{Lu}\p{Nd}_]*") {
+        println!("variable_from_str: {:?}", s);
+        assert!(Variable::is_valid(&s));
+        assert!(Variable::from_str(&s).is_ok());
+    }
+
+    #[test]
+    fn constant_identifier_from_str(s in r"\p{Ll}[\p{Ll}\p{Lu}\p{Nd}_]*(:[\p{Ll}\p{Lu}][\p{Ll}\p{Lu}\p{Nd}_]*)?") {
+        println!("constant_identifier_from_str: {:?}", s);
+        assert!(Constant::is_identifier(&s));
+    }
+}
