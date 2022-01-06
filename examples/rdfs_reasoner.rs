400b6250b4fa57f8c0f83a50e20d2d60a82e87d1
Simon Johnston <johnstonskj@gmail.com>
2022-01-06 07:24:33 -0800

    Made a copy of the RDF test as a stand-alone example.
    Signed-off-by: Simon Johnston <johnstonskj@gmail.com>

---
+++ examples/rdfs_reasoner.rs
@@ -0,0 +1,22 @@
+use asdi::idb::{Evaluator, NaiveEvaluator};
+use asdi::io::{print_relation, Format};
+use asdi::parse::parse_file;
+use asdi::{Collection, Labeled};
+
+fn main() {
+    let mut program = parse_file("examples/reasoning.dl").unwrap().into_parsed();
+
+    program.load_extensional_data().unwrap();
+
+    let evaluator = NaiveEvaluator::default();
+
+    let results = evaluator.inference(&program);
+    assert!(results.is_ok());
+    let new_relations = results.unwrap();
+
+    for relation in new_relations.iter() {
+        println!("{}", relation.label_ref());
+        print_relation(relation, &Format::Text).unwrap();
+    }
+    //print!("{}", program);
+}
